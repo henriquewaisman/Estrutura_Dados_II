620aa54c303362fb8df9f024f55f6730ea04d27d
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2024-08-26 14:46:58 -0300

    Update tree.c

--- Exercícios/tree.c
+++ Exercícios/tree.c
@@ -1,46 +1,96 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct Node{
+typedef struct node {
   int data;
   struct node *left;
   struct node *right;
-};
+} node;
 
-typedef struct Node *BinaryTree;
-
-BinaryTree *createTree(){
-  BinaryTree *root = (BinaryTree *)malloc(sizeof(BinaryTree));
-  if (root != NULL){
-    *root = NULL; 
+node *createTree(int data) {
+  node *newNode = malloc(sizeof(node));
+  if (newNode != NULL) {
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->data = data;
   }
-  return root;
+  return newNode;
 }
 
-// TODO: Insert node
-bool insertNode(BinaryTree *root, int data){
+void printTabs(int numtabs) {
+  for (int i = 0; i < numtabs; i++) {
+    printf("\t");
+  }
+}
+
+void printTree_Rec(node *root, int level) {
+  if (root == NULL) {
+    printTabs(level);
+    printf("--<empty>--\n");
+    return;
+  }
+  printTabs(level);
+  printf("data = %d\n", root->data);
+  printTabs(level);
+  printf("left\n");
+  printTree_Rec(root->left, level + 1);
+  printTabs(level);
+  printf("right\n");
+  printTree_Rec(root->right, level +1);
+  printTabs(level);
+  printf("done\n");
+}
+
+void printTree(node* root){
+  printTree_Rec(root, 0);
+}
+
+bool insertNode(node **rootptr, int data){
+  node *root = *rootptr;
+  if (root == NULL) {
+    (*rootptr) = createTree(data);
+    return true;
+  }
+  if (data == root->data) {
+    return false;
+  }
+  if (data < root->data) {
+    return insertNode(&(root->left), data);
+  } else {
+    return insertNode(&(root->right), data);
+  }
+}
+
+bool findNode(node * root, int data){
   if(root == NULL){
-    return 0;
+    return false;
   }
-  struct Node *newNode;
-  newNode = (struct Node *)malloc(sizeof(struct Node *));
-  if (newNode == NULL) {
-    return 0;
+  if(root->data == data){
+    return true;
   }
-
-  newNode->data = data;
-  newNode->left = NULL;
-  newNode->right = NULL;
-  
-  
+  if(data < root->data){
+    return findNode(root->left, data);
+  }
+  else{
+    return findNode(root->right, data);
+  }
 }
-// TODO: Search node
-
-// TODO: Remove node
 
 int main(void) {
-  createTree();
+  node *root = NULL;
+  insertNode(&root, 15);
+  insertNode(&root, 11);
+  insertNode(&root, 24);
+  insertNode(&root, 5);
+  insertNode(&root, 19);
+  insertNode(&root, 16);
+
+  printTree(root);
+
+  printf("%d (%d)\n", 16, findNode(root, 16));
+  printf("%d (%d)\n", 15, findNode(root, 15));
+  printf("%d (%d)\n", 55, findNode(root, 55));
+  printf("%d (%d)\n", 166, findNode(root, 166));
   return 0;
 }
