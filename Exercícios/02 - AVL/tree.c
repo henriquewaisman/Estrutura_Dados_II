ddd96d28a831a7abb6d23fc97731bbf07bb7802d
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2024-09-14 00:37:16 -0300

    Update tree.c

--- Exercícios/02 - AVL/tree.c
+++ Exercícios/02 - AVL/tree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Criação da estrutura do nó com um dado inteiro, um filho na esquerda e outro na direita. Além da altura inteira.
 typedef struct node {
     int data;
     struct node *left;
@@ -8,8 +9,10 @@ typedef struct node {
     int height;
 } node;
 
+//Cria um nó alocando espaço na memória. 
 struct node *createNode(int data) {
-    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    node *newNode = malloc(sizeof(struct node));
+    //seta os filhos do nó como nulos, sua altura como 0 e o dado é o parâmetro de entrada da função.
     if (newNode != NULL) {
       newNode->data = data;
       newNode->left = NULL;
@@ -19,53 +22,72 @@ struct node *createNode(int data) {
     return newNode;
 }
 
+//seta a altura do nó
 int height(struct node *node) {
-    if (node == NULL) return -1;
+    //Para o nó nulo (após a folha), sua altura é -1 
+    if (node == NULL){
+        return -1;
+    }
     return node->height;
 }
 
+//calcula do fator de balanceamento
 int balanceFactor(struct node *node) {
-    if (node == NULL) return 0;
+    //caso o nó seja nulo, seu fator é 0
+    if (node == NULL){
+        return 0;
+    }
+    //caso não seja, seu fator é altura esquerda - altura direita
     return height(node->left) - height(node->right);
 }
 
+//Rodação direita
 struct node *rightRotation(struct node *node) {
-    struct node *newRoot = node->left;
-    struct node *subTree = newRoot->right;
+    struct node *newRoot = node->left;     // O novo nó raiz será o filho esquerdo do nó desbalanceado.
+    struct node *subTree = newRoot->right; // A subárvore direita do novo nó raiz se tornará a subárvore esquerda do nó original.
+    newRoot->right = node;                 // Ajusta o filho direito do novo nó raiz para ser o nó desbalanceado.
+    node->left = subTree;                  // Atualiza o filho esquerdo do nó desbalanceado para ser a subárvore que foi deslocada.
 
-    newRoot->right = node;
-    node->left = subTree;
-
-    if (height(node->left) > height(node->right)) 
+    // Atualiza a altura do nó desbalanceado (antiga raiz) após a rotação
+    if (height(node->left) > height(node->right)){ 
         node->height = 1 + height(node->left);
-    else 
+    }
+    else{ 
         node->height = 1 + height(node->right);
-
-    if (height(newRoot->left) > height(newRoot->right)) 
+    }
+    // Atualiza a altura do novo nó raiz após a rotação.
+    if (height(newRoot->left) > height(newRoot->right)){ 
         newRoot->height = 1 + height(newRoot->left);
-    else 
+    }
+    else{ 
         newRoot->height = 1 + height(newRoot->right);
-
+    }
+    // Retorna o novo nó raiz após a rotação.
     return newRoot;
 }
 
+//Rotação esquerda
 struct node *leftRotation(struct node *node) {
-    struct node *newRoot = node->right;
-    struct node *subTree = newRoot->left;
+    struct node *newRoot = node->right;     // Realiza uma rotação à esquerda em torno de um nó desbalanceado para balancear a árvore.
+    struct node *subTree = newRoot->left;   // O novo nó raiz será o filho direito do nó desbalanceado.
+    newRoot->left = node;                   // Ajusta o filho esquerdo do novo nó raiz para ser o nó desbalanceado.
+    node->right = subTree;                  // Atualiza o filho direito do nó desbalanceado para ser a subárvore que foi deslocada.
 
-    newRoot->left = node;
-    node->right = subTree;
-
-    if (height(node->left) > height(node->right)) 
+    // Atualiza a altura do nó desbalanceado (antiga raiz) após a rotação.
+    if (height(node->left) > height(node->right)){ 
         node->height = 1 + height(node->left);
-    else 
+    }
+    else{
         node->height = 1 + height(node->right);
-
-    if (height(newRoot->left) > height(newRoot->right)) 
+    }
+    // Atualiza a altura do novo nó raiz após a rotação.
+    if (height(newRoot->left) > height(newRoot->right)){ 
         newRoot->height = 1 + height(newRoot->left);
-    else 
+    }
+    else{ 
         newRoot->height = 1 + height(newRoot->right);
-
+    }
+    // Retorna o novo nó raiz após a rotação.
     return newRoot;
 }
 
@@ -73,49 +95,55 @@ struct node *balance(struct node *root, int data) {
     if (root == NULL) {
         return root;
     }
+    // Atualiza a altura do nó corrente (raiz) com base nas alturas das subárvores esquerda e direita.
     if (height(root->left) > height(root->right)) {
         root->height = 1 + height(root->left);
     } else {
         root->height = 1 + height(root->right);
     }
-
+    // Calcula o fator de balanceamento do nó corrente.
     int balance = balanceFactor(root);
-
+    // Caso o esbalanceamento seja para a esquerda, e inserção seja na esquerda-esquerda: RSD:
     if (balance > 1 && data < root->left->data) 
         return rightRotation(root);
-
+    // Caso o desbalanceamento seja para a direita, e inserção seja na direita-direita: RSE.
     if (balance < -1 && data > root->right->data) 
         return leftRotation(root);
-
+    // Caso o desbalanceamento seja para a esquerda,e inserção seja na esquerda-direita: RDD.
     if (balance > 1 && data > root->left->data) {
         root->left = leftRotation(root->left);
         return rightRotation(root);
     }
-
+    // Caso o desbalanceamento seja para a esquerda,e inserção seja na direita-esquerda: RDE.
     if (balance < -1 && data < root->right->data) {
         root->right = rightRotation(root->right);
         return leftRotation(root);
     }
-
     return root;
 }
 
 struct node *insertNode(struct node *root, int data) {
-    if (root == NULL) {
+    //Se o nó for nulo, crie o nó
+    if (root == NULL){
         return createNode(data);
     }
-
-    if (data < root->data) {
+    //Se o dado for menor que o nó atual, caminhe para a esquerda
+    if (data < root->data){
         root->left = insertNode(root->left, data);
-    } else if (data > root->data) {
+    } 
+    //Se o dado for maior que o nó atual, caminhe para a direita
+    else if (data > root->data){
         root->right = insertNode(root->right, data);
-    } else {
+    } 
+    //Caso o valor do nó já esteja na árovre, retone o nó
+    else{
         return root;
     }
-
+    //Balancear a cada inserção
     return balance(root, data);
 }
 
+//Acha o menor nó da árvore caminhando para a esquerda
 struct node *findMin(struct node *node) {
     struct node *currentNode = node;
     while (currentNode && currentNode->left != NULL)
@@ -123,6 +151,7 @@ struct node *findMin(struct node *node) {
     return currentNode;
 }
 
+//Acha o maior nó da árvore caminhando para a direita
 struct node *findMax(struct node *node) {
     if (node == NULL) return NULL;
     while (node->right != NULL) {
@@ -135,49 +164,62 @@ struct node *removeNode(struct node *root, int data) {
     if (root == NULL) {
         return root;
     }
-
+    // Caso o valor a ser removido seja menor que o valor da raiz, continua a busca pela esquerda.
     if (data < root->data) {
         root->left = removeNode(root->left, data);
-    } else if (data > root->data) {
+    } 
+    // Caso o valor a ser removido seja maior que o valor da raiz, continua a busca pela direita.
+    else if (data > root->data) {
         root->right = removeNode(root->right, data);
-    } else {
+    } 
+    // Se o valor a ser removido for igual ao valor da raiz, esse é o nó a ser removido.
+    else {
+        // Caso o nó não tenha filhos ou tenha apenas um filho à direita.
         if (root->left == NULL) {
             struct node *temp = root->right;
             free(root);
             return temp;
-        } else if (root->right == NULL) {
+        }
+        // Caso o nó não tenha filhos ou tenha apenas um filho à esquerda.
+        else if (root->right == NULL) {
             struct node *temp = root->left;
             free(root);
             return temp;
         }
-
+         // Caso o nó tenha dois filhos. Se a subárvore esquerda for mais alta ou igual, encontra o maior valor da subárvore esquerda.
         if (height(root->left) >= height(root->right)) {
             struct node *temp = findMax(root->left);
             root->data = temp->data;
             root->left = removeNode(root->left, temp->data);
-        } else {
+        }
+        // Se a subárvore direita for mais alta, encontra o menor valor da subárvore direita.
+        else {
             struct node *temp = findMin(root->right);
             root->data = temp->data;
             root->right = removeNode(root->right, temp->data);
         }
     }
-
+    //Balanceia a cada remoção
     return balance(root, data);
 }
 
+//Acha a altura do nó/sub-árovre
 int findHeight(struct node *node) {
     if (node == NULL) {
         return -1;
     }
+    //incrementa a altura a cada nó passado
     int left_height = findHeight(node->left) + 1;
     int right_height = findHeight(node->right) + 1;
-    if (1 + ((right_height > right_height))) {
-        return right_height;
+    //Retorna a maior altura
+    if (1 + ((left_height > right_height))) {
+        return left_height;
     } else {
         return right_height;
     }
 }
 
+//Caminha na árvore para achar um nó, comparando o dado procurado com cada nó atual e caminhando na árvore
 node *findNode(struct node *root, int data) {
     if (root == NULL || root->data == data) 
         return root;
@@ -188,6 +230,7 @@ node *findNode(struct node *root, int data) {
         return findNode(root->right, data);
 }
 
+//Funções alvulsas de print:
 void printTabs(int numtabs) {
     for (int i = 0; i < numtabs; i++) {
         printf("\t");
